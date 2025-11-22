00202046a07b32d8878584fd551b038a04ced120
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-22 16:08:26 +0800

    Setup backend project

---
+++ backend/src/main/java/dev/ailuruslabs/emmaserver/EmmaServerApplication.java
@@ -0,0 +1,13 @@
+package dev.ailuruslabs.emmaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmmaServerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/ailuruslabs/emmaserver/EmmaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ailuruslabs.emmaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
