043d0103f05b6b6ba64204d3607a64bfe68317a6
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-25 10:59:33 +0800

    Add initial IncidentService logic

--- backend/src/main/java/dev/ailuruslabs/emmaserver/incidents/IncidentService.java
+++ backend/src/main/java/dev/ailuruslabs/emmaserver/incidents/IncidentService.java
@@ -1,18 +1,60 @@
 package dev.ailuruslabs.emmaserver.incidents;
 
 import jakarta.validation.Valid;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class IncidentService {
 
     private final IncidentRepository incidentRepository;
+    private final GeometryFactory geometryFactory;
 
-    IncidentService(IncidentRepository incidentRepository) {this.incidentRepository = incidentRepository;}
+    IncidentService(IncidentRepository incidentRepository, GeometryFactory geometryFactory) {
+        this.incidentRepository = incidentRepository;
+        this.geometryFactory = geometryFactory;
+    }
 
-    public Incident saveIncident(@Valid IncidentCreateRequest incidentCreateRequest) {
-        // save incident to repo here
-        // empty for now
-        return new Incident();
+    public Incident saveIncident(IncidentCreateRequest incidentCreateRequest) {
+        var userPrincipal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        UUID userUUID;
+        if (userPrincipal instanceof Jwt jwt) {
+            try {
+                userUUID = UUID.fromString(jwt.getSubject());
+            } catch (IllegalArgumentException iae) {
+                throw new JwtException("Invalid UUID in JWT");
+            }
+        } else throw new JwtException("Invalid JWT");
+
+        var incidentPoint = geometryFactory.createPoint(
+            new Coordinate(
+                incidentCreateRequest.coordinates().longitude(),
+                incidentCreateRequest.coordinates().latitude()
+            )
+        );
+
+        return incidentRepository.save(
+            new Incident(
+                userUUID,
+                incidentPoint,
+                incidentCreateRequest.title(),
+                incidentCreateRequest.description(),
+                incidentCreateRequest.type()
+            )
+        );
+    }
+
+    @Bean
+    GeometryFactory geometryFactory() {
+        return new GeometryFactory(new PrecisionModel(), 4326);
     }
 }
