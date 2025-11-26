a389d8f3788eb9fee326e873b6f82e7f573a0a40
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-26 12:22:15 +0800

    Add findNearbyWithinRadius method to IncidentRepo

--- backend/src/main/java/dev/ailuruslabs/emmaserver/incidents/IncidentRepository.java
+++ backend/src/main/java/dev/ailuruslabs/emmaserver/incidents/IncidentRepository.java
@@ -1,8 +1,19 @@
 package dev.ailuruslabs.emmaserver.incidents;
 
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 interface IncidentRepository extends JpaRepository<Incident, Long> {
+
+    @Query("SELECT i FROM Incident i WHERE ST_DWithin(i.location, :userLocation, :radiusInMeters) = true")
+    List<Incident> findNearbyWithinRadius(
+        @Param("userLocation") Point userLocation,
+        @Param("radiusInMeters") double radiusInMeters
+    );
 }
