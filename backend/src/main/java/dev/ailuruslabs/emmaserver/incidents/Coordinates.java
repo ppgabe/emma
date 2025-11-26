60b2de543b00483d9619f48a668d92ec6703e593
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-11-26 13:44:03 +0800

    Fix longitude validation message

--- backend/src/main/java/dev/ailuruslabs/emmaserver/incidents/Coordinates.java
+++ backend/src/main/java/dev/ailuruslabs/emmaserver/incidents/Coordinates.java
@@ -8,7 +8,7 @@ public record Coordinates(
     @DecimalMax(value = "90", message = "Latitude cannot be greater than 90")
     double lat,
 
-    @DecimalMin(value = "-180", message = "Latitude cannot be less than -180")
-    @DecimalMax(value = "180", message = "Latitude cannot be greater than 180")
+    @DecimalMin(value = "-180", message = "Longitude cannot be less than -180")
+    @DecimalMax(value = "180", message = "Longitude cannot be greater than 180")
     double lon
 ) {}
