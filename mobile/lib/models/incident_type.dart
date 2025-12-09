170553ae05ef8640f9fae12ff1b15834714e5de4
Gabriel Virrey <gabriel@ailuruslabs.dev>
2025-12-09 20:34:53 +0800

    Add IncidentTypeExtension for Incident icons

--- mobile/lib/models/incident_type.dart
+++ mobile/lib/models/incident_type.dart
@@ -1,3 +1,5 @@
+import 'package:emma_mobile/models/incident.dart';
+import 'package:flutter/material.dart';
 
 enum IncidentType {
   EARTHQUAKE,
@@ -6,5 +8,35 @@ enum IncidentType {
   FLOOD,
   ROAD_CRASH,
   ROAD_BLOCK,
-  POWER_OUTAGE;
+  POWER_OUTAGE,
+}
+
+extension IncidentTypeExtension on IncidentType {
+  Icon get icon => switch (this) {
+    IncidentType.EARTHQUAKE => const Icon(
+      Icons.vibration,
+      color: Colors.deepOrangeAccent,
+    ),
+    IncidentType.FIRE => const Icon(
+      Icons.local_fire_department,
+      color: Colors.red,
+    ),
+    IncidentType.FLOOD => const Icon(Icons.flood, color: Colors.blue),
+    IncidentType.LANDSLIDE => const Icon(
+      Icons.landslide,
+      color: Colors.lightGreen,
+    ),
+    IncidentType.POWER_OUTAGE => const Icon(
+      Icons.power_off,
+      color: Colors.blueGrey,
+    ),
+    IncidentType.ROAD_CRASH => const Icon(
+      Icons.car_crash,
+      color: Colors.yellow,
+    ),
+    IncidentType.ROAD_BLOCK => const Icon(
+      Icons.remove_road_outlined,
+      color: Colors.grey,
+    ),
+  };
 }
